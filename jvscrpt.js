4a6834e7777783c0873b76a60b910d291e6eb380
Vugar <isecondbreath@gmail.com>
2023-05-12 14:35:25 +0400

    first commit

---
+++ jvscrpt.js
@@ -0,0 +1,135 @@
+// const num1 =+prompt("1ci ededi daxil edin")
+// const num2 =+prompt("2ci ededi daxil edin")
+// const emeliyyat = prompt("emeliyyat daxil edin")
+// let result = 0;
+
+// switch (emeliyyat) {
+//     case "+":
+//         result = num1 + num2;
+//         break;
+//     case "*":
+//         result = num1 * num2;
+//         break;
+
+//     case "/":
+//         result = num1 / num2;
+//         break;
+//     case "-":
+//         result = num1 - num2;
+//         break;
+//     default:
+//         break;
+// }
+// console.log (result)
+
+// let Celsius = prompt("Enter a temperature in Celsius");
+// let Fahrenheit = Celsius * (9/5) + 32;
+// console.log("The temperature in Fahrenheit is: " + Fahrenheit);
+
+// let first_number = parseFloat(prompt("Enter the first number"));
+// let second_number = parseFloat(prompt("Enter the second number"));
+// let third_number = parseFloat(prompt("Enter the third number"));
+// let result = (first_number + second_number + third_number) / 3;
+
+// console.log("The average result is: " + result);
+
+// let usernumber = parseFloat (prompt('enter a number'));
+// if (usernumber%2==0) {console.log('The number is even')
+
+// } else {console.log('The number is odd')
+
+// }
+
+// let usernumber1 = parseFloat(prompt("Enter 1st number"));
+// let usernumber2 = parseFloat(prompt("Enter 2nd number"));
+// let usernumber3 = parseFloat(prompt("Enter 3rd number"));
+// if (usernumber1>usernumber2&&usernumber3) {console.log("Maximum number is:" +usernumber1)}
+//     if (usernumber2>usernumber1&&usernumber3) {console.log("Maximum number is:" +usernumber2)}
+//     if (usernumber3>usernumber1&&usernumber2) {console.log("Maximum number is:" +usernumber3)
+
+//     }
+
+// let firstnumber = parseFloat(prompt('enter first number:'));
+// let secondnumber = parseFloat(prompt('enter second number:'));
+// let result = firstnumber+secondnumber;
+// console.log(+result);
+
+// let score = parseFloat(prompt('Enter your score'));
+// if (score >= 90) {
+//   console.log('Grade: A');
+// } else if (score >= 80 && score <= 89) {
+//   console.log('Grade: B');
+// } else if (score >= 70 && score <= 79) {
+//   console.log('Grade: C');
+// } else if (score >= 60 && score <= 69) {
+//   console.log('Grade: D');
+// } else {
+//   console.log('Grade: F');
+// }
+
+// let firstname = prompt("Adinizi daxil edin:");
+// let surname = prompt("Soyadinizi daxil edin:");
+// let score1 = parseFloat(prompt("1ci imtahan neticesini daxil edin:"));
+// let score2 = parseFloat(prompt("2ci imtahan neticesini daxil edin:"));
+// let score3 = parseFloat(prompt("3ci imtahan neticesini daxil edin:"));
+
+// if (!score1) {
+//     score1 = 51;
+// }
+
+// if (!score2) {
+//     score2 = 51;
+// }
+
+// if (!score3) {
+//     score3 = 51;
+// }
+
+// if (score1 < 0 || score1 > 100 || score2 < 0 || score2 > 100 || score3 < 0 || score3 > 100) {
+//     console.log("Daxil edilen imtahan neticeleri 0-dan kiçik və ya 100-dən böyük ola bilməz.");
+
+// }
+
+// let average = (score1 + score2 + score3) / 3;
+// console.log('Ad: ' + firstname);
+// console.log('Familiya: ' + surname);
+// console.log('avarage: ' + average);
+
+// if (average>=81) {
+//     console.log("Diplom isine dusur")
+
+// } else {
+//     console.log("Diplom isine dusmur")
+// }
+
+// const arr=[12,'name',true]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// let i = 0; let sum = 0
+// while (i < 100) {
+//   if (i%2==1){
+
+//     sum = sum + i
+//   }i++
+// }
+// console.log(sum)
+
+
+
+// let number1 = Number(prompt("birinci ededi daxil edin"));
+// let number2 = Number(prompt("ikinci ededi daxil edin"));
+
+// let sum = 0
+// for(let i = 0;i < number1; i++){
+//   sum += number2
+// }
+
+// console.log(sum);
+
+
+// for (let i=1 ; i<10; i++){
+//   console.log(i);
+// }
+
