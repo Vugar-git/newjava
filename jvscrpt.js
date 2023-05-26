ab10e1d1ae98cb28c3b028964901cf498206291b
Vugar <isecondbreath@gmail.com>
2023-05-26 17:28:04 +0400

    git commit -m "Your commit message goes here"

--- jvscrpt.js
+++ jvscrpt.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // const num1 =+prompt("1ci ededi daxil edin")
 // const num2 =+prompt("2ci ededi daxil edin")
 // const emeliyyat = prompt("emeliyyat daxil edin")
@@ -149,17 +151,12 @@
 //   console.log(i);
 // }
 
-
-
 // for (let i = 20; i >= 10; i--) {
 //   if (i === 13) {
 //     break;
 //   }
 //   console.log(i);
 // }
-
-
-
 
 // for (let i = 1; i <= 10; i++) {
 //   if (i % 2 === 0) {
@@ -178,7 +175,6 @@
 //    }
 //    i++;
 //  }
-
 
 //  const arrayOfNumbers = [];
 
@@ -210,7 +206,6 @@
 
 // console.log(pattern);
 
-
 // const usdcurr = 1.7;
 // const eurocurr = 1.84;
 
@@ -220,16 +215,484 @@
 
 // convert(1,usdcurr)
 
+// function sayHello(name) {
+//   console.log("Hello " + name);
+// }
 
-function sayHello(name) {
-  console.log("Hello " + name);
-}
+// sayHello("Vugar");
 
-sayHello("Vugar");
+// function qonsu(num) {
+//   return[num-1, num , num+1];
+
+// }
+// console.log(qonsu(5));
+
+// const obj = {
+//   Anna : 500,
+//   Alice : 800
+// }
+
+// console.log(obj.Alice);
+
+// const arr = ['vugar','nurane','arif']
+// console.log(arr[1]);
+
+// const user = 'vugar';
+// console.log(`salam  ${user}`);
+
+// const user = 'vugar';
+// console.log("salam " + user);
+
+// let numberOfFilms = prompt("how many movies u seen?");
+// let personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
+// let answer1 = prompt("Last movie you watched?");
+// let answer2 = prompt("Rating of the last movie?");
+
+// personalMovieDB.movies[answer1]=answer2;
+// console.log(personalMovieDB);
+
+// function showmessage(text) {
+//     console.log(text);
+// }
+
+// showmessage('salam');
+
+// let question = prompt("yasinizi daxil edin");
+// if (question > 17) {
+//   alert("Access granted");
+// } else {
+//   alert("Access denied");
+// }
+
+//tasks
+
+// let V = +prompt("İkirəqəmli ədəd daxil edin");
+
+// while (V < 100 || V > 999) {
+//   V += 7;
+// }
+
+// console.log("Son dəyər: " + V);
+
+// let N = +prompt("Rəqəm daxil edin");
+
+// for (let i = 0; i < N; i++) {
+//   console.log("I know how to use cycles");
+// }
+
+// for (let i = 100; i < 1000; i++) {
+//   if (i % 10 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// let sum = 0;
+
+// for (let i = 10; i < 100; i++) {
+//   if (i % 2 === 1) {
+//     sum += i;
+//   }
+// }
+
+// console.log("Cəm: " + sum);
+
+// let bolunen = +prompt("Boluneni daxil edin");
+// let sum = 0;
+
+// for (let i = 100; i < 1000; i++) {
+//   if (i % bolunen === 0) {
+//     sum += i;
+//   }
+// }
+
+// console.log("Cəm:", sum);
+
+// const arr = ['D', 'A', 'B', 'Vugar', 'i'];
+// const element = arr.sort()
+
+// console.log(element);
+
+// const arr = ['D', 'A', 'B', 'Vugar', 'i'];
+// const element = arr.sort()
+
+// console.log(arr.some((element)=>element ==='Vugar'));
+
+// let arr = [46, 67, 78, 546, 6466];
+// console.log(arr.filter(item => item < 100));
+
+// let arr1 = [2, 4, 6, 8];
+
+// let newArr = arr1.map((item) => item + 2);
+
+// console.log(newArr);
+
+// task1
+// let numbers = [1, 5, -7, 3, -9, 4, -6, 2];
+
+// let modNumbers = numbers.map((num) => {
+//   if (num < 0) {
+//     return -num;
+//   } else {
+//     return num * 2;
+//   }
+// });
+
+// console.log(modNumbers);
+
+// task3
+
+// let names = ["Sam", "Alan", "Bill", "Adam", "Anna", "George"];
+
+// let filteredNames = names.filter((name) => name.concat );
+
+// console.log(filteredNames);
+
+// const options = {
+//   name: "test",
+//   width: 1024,
+//   height: 1024,
+//   color: {
+//     border: "black",
+//     bg: "red",
+//   },
+// };
+
+// console.log(Object.keys(options).length);
+
+// for (let key in options) {
+//   if (typeof options[key] === "object") {
+//     for (let i in options[key]) {
+//       console.log(`Property ${i} for ${options[key][i]}`);
+//     }
+//   } else {
+//     console.log(`Property ${key} for ${options[key]}`);
+//   }
+// }
+
+// delete options.name;
+
+// console.log(options);
+
+// const people = [
+//   { firstname: "Vugar", age: 25, budget: 10000 },
+//   { firstname: "Ali", age: 23, budget: 20000 },
+//   { firstname: "Cingiz", age: 21, budget: 9000 },
+// ];
+
+// // for ( let pop of people){
+// //     console.log(pop);
+// // }
+
+// // for ( let i = 0 ; i<people.length;i++){
+// //     console.log(people[i]);
+// // }
+
+// people.forEach(person => console.log(person))
+
+// const personalPlanPeter = {
+//   name: "Peter",
+//   age: "29",
+//   skills: {
+//     languages: ["ru", "eng"],
+//     programmingLangs: {
+//       js: "20%",
+//       php: "10%",
+//       html: "100%",
+//     },
+//     exp: "1 month",
+//   },
+//   showAgeAndLangs: function () {
+//     const age = this.age;
+//     const languages = this.skills.languages
+//       .map((lang) => lang.toUpperCase())
+//       .join(", ");
+//     return `My age is ${age} and I know: ${languages}`;
+//   },
+// };
+
+// console.log(personalPlanPeter.showAgeAndLangs());
+
+// function showProgrammingLangs(plan) {
+//   const programmingLangs = personalPlanPeter.skills.programmingLangs;
+
+//   let output = "";
+
+//   for (let lang in programmingLangs) {
+//     if (programmingLangs.hasOwnProperty(lang)) {
+//       output += `Язык ${lang} изучен на ${programmingLangs[lang]}. `;
+//     }
+//   }
+
+//   console.log(output);
+// }
+
+// showProgrammingLangs(personalPlanPeter);
+
+// function showExperience(plan) {
+//   console.log(plan);
+// }
+
+// showExperience(personalPlanPeter.skills.exp);
+
+// const family = ["Peter", "Ann", "Alex", "Linda"];
+
+// function showFamily(arr) {
+//   if (arr.length === 0) {
+//     console.log("Семья пуста");
+//   } else {
+//     console.log("Семья состоит из: ", arr);
+//   }
+// }
+
+// showFamily(family);
+
+// const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+// function standardizeStrings(arr) {
+//   arr.forEach((city) => {
+//     console.log(city.toLowerCase());
+//   });
+// }
+
+// standardizeStrings(favoriteCities);
+
+// const someString = 'salam';
+
+// function reverse(str) {
+//   if (typeof str !== "string" || str === null || str === undefined) {
+//     console.log("Ошибка!");
+//   } else {
+//    return str.split("").reverse().join("");
+//   }
+// }
+
+// console.log(reverse(someString));
+
+// const someString = "salam";
+
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+
+// }
+
+// console.log(reverse(someString));
+
+// Task 1-
+// const string =
+//   "Lorem ipsum is a placeholder text commonly used to lorem demonstrate the visual form of a document or a typeface without relying on meaningful lorem content.";
+
+// function modified(sentence) {
+//   const capitalizeSentence =
+//     sentence.charAt(0).toUpperCase() + sentence.slice(1);
+
+//     const replaced = capitalizeSentence.replace(/lorem/gi,"graphic");
+
+//   return replaced;
+// }
+
+// console.log(modified(string));
+
+// Task 2+
+
+// const arr = [
+//   "Black Sea",
+//   "Caribbean Sea",
+//   "North Sea",
+//   "Baltic Sea",
+//   "Red Sea",
+// ];
+
+// const modifiedseas = arr.map(arr =>arr.replace("Sea",""))
+// console.log(modifiedseas);
+
+// Task 4+
+
+// const arr = [
+//   { name: "miki", surname: "Jonatan", age: "21" },
+//   { name: "miki", surname: "Jonatan", age: "21" },
+//   { name: "miki", surname: "Jonatan", age: "21" },
+// ];
+
+// const capitalized = arr.map(obj =>({
+//     name : obj.name.toUpperCase(),
+//     surame : obj.surname.toUpperCase(),
+//     age : parseFloat(obj.age)
+// }
+
+// ))
+
+// console.log(capitalized);
+
+// Task 5+
+
+// let d1 = "2015-03-25"
+// let d2 = "2015-06-25"
+// let d3 = "2015-03-15"
+
+// if (d1>d2 && d1>d3) {
+//     console.log('d1 largest one');
+
+// }
+// else if(d2>d1 && d2>d3){
+//     console.log("d2 largest one");
+// }
+// else if(d3>d1 && d3>d2){
+//     console.log("d3 largest one");
+// }
+
+// Task 10
+
+// const array = [
+//   { person: { age: "29", salary: "300" } },
+//   { person: { age: "19", salary: "400" } },
+//   { person: { age: "9", salary: "500" } },
+//   { person: { age: "18", salary: "600" } },
+// ];
+
+// function calculateTotalSalary(arr) {
+//   let totalSalary = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     const age = parseInt(arr[i].person.age);
+
+//     if (age > 18) {
+//       const salary = parseInt(arr[i].person.salary);
+//       totalSalary += salary;
+//     }
+//   }
+
+//   return totalSalary;
+// }
+
+// const totalSalary = calculateTotalSalary(array);
+// console.log(totalSalary); // Output: 900
+
+// Task 11
+
+// const fruits = [
+//   "Apple",
+//   "Banana",
+//   "Strawberry",
+//   "Banana",
+//   "Mango",
+//   "Cherry",
+//   "Banana",
+//   "Mango",
+// ];
+
+// const uniqueFruits = [...new Set(fruits)];
+
+// console.log(uniqueFruits);
+
+// task 12
+
+// const demoData = {
+//   data: [
+//     {
+//       period: "Month",
+//       start_date: "2012-06",
+//       end_date: "2012-07",
+//       attributes: {},
+//       measures: {
+//         Visits: 1000000,
+//       },
+//       SubRows: [
+//         {
+//           country: "Albania",
+//           measures: {
+//             Visits: 50,
+//             countryCode: "67",
+//           },
+//           SubRows: null,
+//         },
+//         {
+//           country: "Germany",
+//           measures: {
+//             Visits: 300,
+//             countryCode: "154",
+//           },
+//           SubRows: null,
+//         },
+//         {
+//           country: "Azerbaijan",
+//           measures: {
+//             Visits: 100,
+//             countryCode: "994",
+//           },
+//           SubRows: null,
+//         },
+//       ],
+//     },
+//   ],
+// };
+
+// const countrycodefind = demoData.data[0].SubRows.filter((item) => item.country === "Azerbaijan"
+// );
+// console.log(countrycodefind);
+
+// const info = {
+//   firstname: "Vugar",
+//   surname: "Mammadov",
+//   birthdate: prompt("Enter birthdate: "),
+//   age: function () {
+//     return 2023 - parseInt(this.birthdate);
+//   },
+// };
+
+// console.log(info.age());
+
+// task-1
+
+// let user = {
+//     firstname:"Vugar",
+//     gender: 'male',
+//     birthdate: 1990
+// }
+
+// console.log(user.birthdate);
+
+//task-2
+
+// let user = {
+//     firstname:"Vugar",
+//     gender: 'male',
+//     birthdate: 1990
+// }
+
+// user.age = 29;
+// delete user.birthdate;
+// console.log(user);
+
+//task-4
+
+// const obj = {
+//   retirement: 65,
+//   getYearsBeforeRetirement: function (age) {
+//     return this.retirement - age;
+//   },
+// };
+
+// console.log(obj.getYearsBeforeRetirement(40));
 
 
-function qonsu(num) {
-  return[num-1, num , num+1];
-    
-}
-console.log(qonsu(5));
+// class Product {
+//   constructor(name, price, color) {
+//     (this.Name = name), (this.Price = price), (this.color = color);
+//   }
+// }
+
+// class Tv extends Product {
+//   constructor(screenSize, brand, name,price, color) {
+//     super(name,price, color);
+//     (this.ScreeenSize = screenSize), (this.Brand = brand);
+//   }
+// }
+
+// const tv = new Tv("108", "LG", 450, "black");
+// console.log(tv);
---
+++ tasks.js
@@ -0,0 +1,102 @@
+"use strict";
+
+//task1
+// function logGreeting(firstname) {
+//   console.log(`${firstname} is a JS developer`);
+// }
+
+// logGreeting("Vugar");
+
+
+//task2
+// function user(firstname) {
+//     console.log(firstname + "is a JS developer");
+// }
+
+// user("Vugar ")
+// user("Cavid ")
+// user("Ali ")
+
+//task3
+// function findmaxnumber(a,b,c) {
+//     let max = a;
+//     if (b>max) {
+// max=b
+//     }
+//     if (c>max) {
+//         max=c
+//     }
+// return max;
+// }
+
+// let maxnumber = findmaxnumber(5748,5797,6270);
+// console.log(maxnumber);
+
+//task4
+// let question = prompt(
+//   "JavaScript-də funksiya təyin etmək üçün hansı sintaksis istifadə olunur? a) func b) method c) class d) function"
+// );
+
+// if (question === "d") {
+//   alert("Siz qalib gəldiniz");
+// } else {
+//   alert("Siz məğlub oldunuz");
+// }
+
+//task5
+
+// function min(a, b) {
+//   if (a < b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+
+// let result = min(564,89);
+// console.log(result);
+
+
+//task6
+
+// function calc(a, b, emeliyyat) {
+//   switch (emeliyyat) {
+//     case "+":
+//       return a + b;
+//     case "-":
+//       return a - b;
+//     case "*":
+//       return a * b;
+//     case "/":
+//       return a / b;
+//     default:
+//       return "Düzgün riyazi əməliyyat işarəsi daxil edin.";
+//   }
+// }
+
+// console.log(calc(5,7,'*'));
+              
+
+
+
+// Task2
+// let question = prompt(
+//   "JavaScript-də funksiya təyin etmək üçün hansı sintaksis istifadə olunur? a) func b) method c) class d) function"
+// );
+// let question2 = prompt(
+//   "Bakinin paytaxti hansi sheherdir? a) Baki b) Shamaxi c) Gence d) Qazax"
+// );
+// let question3 = prompt(
+//   "Turkiyenin paytaxti hansi sheherdir? a) Istanbul b) Ankara c) Izmir d) Antalya"
+// );
+
+
+// if (question === "d" && question2 ==='a' && question3 ==='b') {
+//   alert("Siz qalib gəldiniz");
+// } else {
+//   alert("Siz məğlub oldunuz");
+// }
+
+
+
+
